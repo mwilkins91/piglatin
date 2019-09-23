376f0c830f30f2dfe1bddbab90fa9ffab9671407
Mark Wilkins <contactme@markwilkins.co>
2019-09-22 22:42:32 -0400

    pig latin, basic working

--- piglatin.py
+++ piglatin.py
@@ -1,7 +1,19 @@
+vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 
-while True:
-    ans = input('Continue?')
-    if (ans == 'y'):
-        continue
-    else:
-        exit(0)
+
+def pigify(text):
+    words = text.split(' ')
+    piglatinWords = []
+
+    for word in words:
+        if (word[0] in vowels):
+            pigWord = word + 'ay'
+        else:
+            pigWord = word[1:] + word[0] + 'ay'
+        piglatinWords.extend([pigWord])
+    return ' '.join(piglatinWords)
+
+
+text = input('Give me a word or phrase:  ')
+print('Your text in piglatin: ')
+print(pigify(text))
